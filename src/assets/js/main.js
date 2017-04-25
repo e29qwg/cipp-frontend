d1aecd870a2212108914bf8a66e8f9fc62538dfb
Kullawat Chaowanawatee <e29qwg@gmail.com>
2017-04-25 11:54:19 +0700

    add service worker for PWA, optimise logo file and adjust manifest

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,3 +4,14 @@ $(document).ready(function() {
     draggable: true
   });
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+}
